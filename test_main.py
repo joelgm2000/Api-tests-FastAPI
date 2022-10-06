7d0ff3fc34cbc54f0ca0f0093e117ad691723501
unknown <jbgarcia@unicesar.edu.co>
2022-10-05 22:02:21 -0500

    init__

--- test_main.py
+++ test_main.py
@@ -17,7 +17,7 @@ def test_read_isPrimo1():
 
 
 def test_read_isPrimo2():
-    response = client.get("/isPrimo/6")
+    response = client.get("/isPrimo/-2")
     assert response.status_code == 200
     assert response.json() == {"changed": False, "msg": "No es primo"} 
 
@@ -25,3 +25,8 @@ def test_read_isPrimo3():
     response = client.get("/isPrimo/f")
     assert response.status_code == 422
     assert response.json() == {"detail":[{"loc":["path","is_primo_id"],"msg":"value is not a valid integer","type":"type_error.integer"}]}   
+
+def test_read_isPrimo4():
+    response = client.get("/isPrimo/1")
+    assert response.status_code == 200
+    assert response.json() == {"changed": False, "msg": "No es primo"}   
